a82dffe52ebae41a34f1d73c8b87026c6771fe91
Ravindra Kumar <ravidsrk@gmail.com>
2017-11-07 22:36:23 +0530

    Synced latest changes for google-samples

--- templates/googlesamples-mvp/app/src/prod/java/com/example/android/architecture/blueprints/todoapp/Injection.java
+++ templates/googlesamples-mvp/app/src/prod/java/com/example/android/architecture/blueprints/todoapp/Injection.java
@@ -19,10 +19,12 @@ package <%= appPackage %>;
 import android.content.Context;
 import android.support.annotation.NonNull;
 
-import <%= appPackage %>.data.source.local.TasksLocalDataSource;
 import <%= appPackage %>.data.source.TasksDataSource;
-import <%= appPackage %>.data.source.remote.TasksRemoteDataSource;
 import <%= appPackage %>.data.source.TasksRepository;
+import <%= appPackage %>.data.source.local.TasksLocalDataSource;
+import <%= appPackage %>.data.source.local.ToDoDatabase;
+import <%= appPackage %>.data.source.remote.TasksRemoteDataSource;
+import <%= appPackage %>.util.AppExecutors;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
@@ -34,7 +36,9 @@ public class Injection {
 
     public static TasksRepository provideTasksRepository(@NonNull Context context) {
         checkNotNull(context);
+        ToDoDatabase database = ToDoDatabase.getInstance(context);
         return TasksRepository.getInstance(TasksRemoteDataSource.getInstance(),
-                TasksLocalDataSource.getInstance(context));
+                TasksLocalDataSource.getInstance(new AppExecutors(),
+                        database.taskDao()));
     }
 }
